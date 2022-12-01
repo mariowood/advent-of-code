c45b0beb128d277f87e354516ce62dd91216b73e
Andrew Marwood <andrew.marwood@justeattakeaway.com>
2022-12-01 20:49:53 +0000

    Advent of Code day 1

---
+++ PuzzleSolver/Days/December1st.cs
@@ -0,0 +1,52 @@
+﻿namespace PuzzleSolver.Days;
+
+public class December1st
+{
+    private readonly Dictionary<int, int> _caloriesPerElf = new();
+
+    public December1st(List<string> lines)
+    {
+        GetCaloriesForElf(lines, 0);
+    }
+
+    public int GetTopThreeElvesTotalCalories()
+    {
+        var elfList = _caloriesPerElf.ToList();
+        elfList.Sort((x, y) => x.Value.CompareTo(y.Value));
+        var topThree = elfList.TakeLast(3);
+        return topThree.Sum(elf => elf.Value);
+    }
+    
+    public KeyValuePair<int, int> GetElfWithMostCalories()
+    {
+        int maxCalories = _caloriesPerElf.Values.Max();
+        KeyValuePair<int, int> elf = _caloriesPerElf.FirstOrDefault(e => e.Value == maxCalories);
+        return elf;
+    }
+
+    private void GetCaloriesForElf(List<string> lines, int index)
+    {
+        int calories = 0;
+
+        while (index < lines.Count && !string.IsNullOrWhiteSpace(lines[index]))
+        {
+            if (int.TryParse(lines[index], out int lineCalories))
+            {
+                calories += lineCalories;
+            }
+
+            index++;
+        }
+
+        index++;
+
+        int numberOfElves = _caloriesPerElf.Count;
+
+        _caloriesPerElf[numberOfElves + 1] = calories;
+
+        if (index < lines.Count)
+        {
+            GetCaloriesForElf(lines, index);
+        }
+    }
+}
---
+++ PuzzleSolver/Program.cs
@@ -0,0 +1,29 @@
+﻿using PuzzleSolver.Days;
+
+Console.WriteLine("Please enter the advent of code day (a number between 1 and 24 inclusive).");
+string? day = Console.ReadLine();
+List<string> lines = File.ReadLines("input.txt").ToList();
+
+if (int.TryParse(day, out int puzzle))
+{
+    switch (puzzle)
+    {
+        case 1:
+            var solver = new December1st(lines);
+            var elf = solver.GetElfWithMostCalories();
+            var topThreeElvesTotalCalories = solver.GetTopThreeElvesTotalCalories();
+            Console.WriteLine($"The elf with the most calories is {elf.Key} with {elf.Value} calories.");
+            Console.WriteLine($"The top 3 elves have a total of {topThreeElvesTotalCalories} calories.");
+            break;
+        default:
+            Console.WriteLine("Unrecognized puzzle day.");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Invalid advent of code day, please enter a number between 1 and 24 inclusive.");
+}
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
