7e481951d5ff81b51b6c309853b60a1dbb3f0e4b
Andrew Marwood <andrew.marwood@justeattakeaway.com>
2022-12-04 22:07:22 +0000

    Write puzzle description to console

--- PuzzleSolver/Constants.cs
+++ PuzzleSolver/Constants.cs
@@ -2,6 +2,7 @@
 
 public static class Constants
 {
-    public const string PartOne = "[bold blue]Pt. 1:[/]";
-    public const string PartTwo = "[bold purple]Pt. 2:[/]";
+    public const string Puzzle = "[bold yellow]Puzzle:[/]";
+    public const string PartOne = "[bold blue]Pt.  1:[/]";
+    public const string PartTwo = "[bold purple]Pt.  2:[/]";
 }
