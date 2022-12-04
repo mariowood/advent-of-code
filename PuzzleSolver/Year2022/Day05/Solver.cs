2443a69e39081dd8265bfe9a86de2ddc1dfab03a
Andrew Marwood <andrew.marwood@justeattakeaway.com>
2022-12-04 21:29:21 +0000

    Add day 5 stub

---
+++ PuzzleSolver/Year2022/Day05/Solver.cs
@@ -0,0 +1,19 @@
+﻿namespace PuzzleSolver.Year2022.Day05;
+
+public class Solver : PuzzleSolver
+{
+    public override void ProcessInput(List<string> lines)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SolvePartOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SolvePartTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
