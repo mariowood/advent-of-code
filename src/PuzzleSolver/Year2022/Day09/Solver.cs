67bc3a380214256c3246326e69b5f09a6e270aca
Andrew Marwood <andrew.marwood@justeattakeaway.com>
2022-12-09 20:28:22 +0000

    Add day 9 scaffolding

---
+++ src/PuzzleSolver/Year2022/Day09/Solver.cs
@@ -0,0 +1,35 @@
+﻿namespace PuzzleSolver.Year2022.Day09;
+
+/// <summary>
+/// A class which will solve the puzzle from https://adventofcode.com/2022/day/8.
+/// </summary>
+[PuzzleDescription(description: "Day 9: Rope Bridge", 2022, 9)]
+public sealed class Solver : PuzzleSolver
+{
+    /// <summary>
+    /// Solves the first part of the puzzle.
+    /// </summary>
+    /// <returns>The answer for part one.</returns>
+    public string SolvePartOne() => "N/A";
+
+    /// <summary>
+    /// Solves the second part of the puzzle.
+    /// </summary>
+    /// <returns>The answer for part two.</returns>
+    public string SolvePartTwo() => "N/A";
+
+    /// <inheritdoc/>
+    public override void ProcessInput(List<string> lines)
+    {
+    }
+
+    /// <inheritdoc/>
+    protected override void SolvePuzzles()
+    {
+        string partOne = SolvePartOne();
+        string partTwo = SolvePartTwo();
+
+        AddPartOneAnswer("Not Implemented.", partOne);
+        AddPartTwoAnswer("Not Implemented.", partTwo);
+    }
+}
