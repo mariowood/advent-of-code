ec45b42fceaa3933b43732f214bc4ee0532c3f39
Andrew Marwood <andrew.marwood@justeattakeaway.com>
2022-12-13 08:05:06 +0000

    Scaffold initial puzzle solver

---
+++ src/PuzzleSolver/Year2022/Day11/Solver.cs
@@ -0,0 +1,43 @@
+﻿namespace PuzzleSolver.Year2022.Day11;
+
+/// <summary>
+/// A class which will solve the puzzle from https://adventofcode.com/2022/day/11.
+/// </summary>
+[PuzzleDescription(description: "Day 10: Cathode-Ray Tube", 2022, 11)]
+public class Solver : PuzzleSolver
+{
+    private readonly List<string> _puzzleInput = new();
+
+    /// <summary>
+    /// Solves the first part of the puzzle.
+    /// </summary>
+    /// <returns>The answer for part one.</returns>
+    public int SolvePartOne()
+    {
+        int result = -1;
+        return result;
+    }
+
+    /// <summary>
+    /// Solves the second part of the puzzle.
+    /// </summary>
+    /// <returns>The answer for part two.</returns>
+    public int SolvePartTwo()
+    {
+        int result = -1;
+        return result;
+    }
+
+    /// <inheritdoc/>
+    public override void ProcessInput(List<string> lines) => _puzzleInput.AddRange(lines);
+
+    /// <inheritdoc/>
+    protected override void SolvePuzzles()
+    {
+        int partOne = SolvePartOne();
+        int partTwo = SolvePartTwo();
+
+        AddPartOneAnswer("Not Implemented.", partOne);
+        AddPartTwoAnswer("Not Implemented.", partTwo);
+    }
+}
---
+++ tests/PuzzleSolver.Tests/Year2022Tests/Day11/SolverTests.cs
@@ -0,0 +1,23 @@
+﻿namespace PuzzleSolver.Tests.Year2022Tests.Day11;
+
+public class SolverTests
+{
+    private const string SampleData = @"";
+
+    [Fact]
+    public void Solves_With_Sample_Data()
+    {
+        // Arrange
+        Year2022.Day11.Solver puzzleSolver = new();
+        List<string> input = SampleData.Split("\r\n").ToList();
+
+        // Act
+        puzzleSolver.ProcessInput(input);
+        int partOne = puzzleSolver.SolvePartOne();
+        int partTwo = puzzleSolver.SolvePartTwo();
+
+        // Assert
+        partOne.ShouldBe(-1);
+        partTwo.ShouldBe(-1);
+    }
+}
