7e5854ff206c92dd5480ecf5e6d0d2000b19e369
Andrew Marwood <andrew.marwood@justeattakeaway.com>
2022-12-13 22:16:41 +0000

    Finish day 11

--- src/PuzzleSolver/Year2022/Day11/Solver.cs
+++ src/PuzzleSolver/Year2022/Day11/Solver.cs
@@ -3,10 +3,11 @@
 /// <summary>
 /// A class which will solve the puzzle from https://adventofcode.com/2022/day/11.
 /// </summary>
-[PuzzleDescription(description: "Day 10: Cathode-Ray Tube", 2022, 11)]
+[PuzzleDescription(description: "Day 11: Monkey in the Middle", 2022, 11)]
 public class Solver : PuzzleSolver
 {
     private readonly List<string> _puzzleInput = new();
+    private readonly List<Monkey> _monkeys = new();
 
     /// <summary>
     /// Solves the first part of the puzzle.
@@ -14,30 +15,149 @@ public class Solver : PuzzleSolver
     /// <returns>The answer for part one.</returns>
     public int SolvePartOne()
     {
-        int result = -1;
-        return result;
+        PopulateMonkeys();
+        ProcessRounds(20);
+        int monkeyBusiness = _monkeys
+            .Select(m => m.InspectedItems)
+            .OrderBy(i => i)
+            .TakeLast(2)
+            .Aggregate(1, (a, b) => a * b);
+        return monkeyBusiness;
     }
 
     /// <summary>
     /// Solves the second part of the puzzle.
     /// </summary>
     /// <returns>The answer for part two.</returns>
-    public int SolvePartTwo()
+    public long SolvePartTwo()
     {
-        int result = -1;
-        return result;
+        PopulateMonkeys(constantWorry: false);
+        ProcessRounds(10000);
+        long monkeyBusiness = _monkeys
+            .Select(m => m.InspectedItems)
+            .OrderBy(i => i)
+            .TakeLast(2)
+            .Aggregate(1L, (a, b) => a * b);
+        return monkeyBusiness;
     }
 
     /// <inheritdoc/>
-    public override void ProcessInput(List<string> lines) => _puzzleInput.AddRange(lines);
+    public override void ProcessInput(List<string> input) => _puzzleInput.AddRange(input);
 
     /// <inheritdoc/>
     protected override void SolvePuzzles()
     {
         int partOne = SolvePartOne();
-        int partTwo = SolvePartTwo();
+        long partTwo = SolvePartTwo();
 
-        AddPartOneAnswer("Not Implemented.", partOne);
-        AddPartTwoAnswer("Not Implemented.", partTwo);
+        AddPartOneAnswer("The level of monkey business after 20 rounds of stuff-slinging simian shenanigans.", partOne);
+        AddPartTwoAnswer("The level of monkey business after 10000 rounds.", partTwo);
+    }
+
+    private void PopulateMonkeys(bool constantWorry = true)
+    {
+        _monkeys.Clear();
+
+        int commonDivisor = 1;
+
+        for (int i = 0; i < _puzzleInput.Count; i += 7)
+        {
+            List<long> startingItems = _puzzleInput[i + 1]
+                .Replace("  Starting items: ", string.Empty)
+                .Split(',')
+                .Select(item => long.Parse(item.Trim()))
+                .ToList();
+            string[] operationParts = _puzzleInput[i + 2]
+                .Replace("  Operation: new = ", string.Empty)
+                .Split(' ');
+
+            long Operation(long x) =>
+                operationParts[1] switch
+                {
+                    "+" => (operationParts[0] == "old" ? x : int.Parse(operationParts[0])) +
+                           (operationParts[2] == "old" ? x : int.Parse(operationParts[2])),
+                    "*" => (operationParts[0] == "old" ? x : int.Parse(operationParts[0])) *
+                           (operationParts[2] == "old" ? x : int.Parse(operationParts[2])),
+                    _ => throw new InvalidOperationException($"Unexpected operand: {operationParts[1]}"),
+                };
+
+            int divisibleBy = int.Parse(
+                _puzzleInput[i + 3].Replace("  Test: divisible by ", string.Empty));
+            int trueMonkey = int.Parse(_puzzleInput[i + 4].Replace("    If true: throw to monkey ", string.Empty));
+            int falseMonkey = int.Parse(_puzzleInput[i + 5].Replace("    If false: throw to monkey ", string.Empty));
+            int Test(long x)
+            {
+                return x % divisibleBy == 0 ? trueMonkey : falseMonkey;
+            }
+
+            commonDivisor *= divisibleBy;
+
+            _monkeys.Add(new Monkey(
+                startingItems,
+                Operation,
+                Test));
+        }
+
+        foreach (Monkey monkey in _monkeys)
+        {
+            if (constantWorry)
+            {
+                monkey.WorryReducer = x => x / 3;
+            }
+            else
+            {
+                monkey.WorryReducer = x => x % commonDivisor;
+            }
+        }
+    }
+
+    private void ProcessRounds(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            ProcessRound();
+        }
+    }
+
+    private void ProcessRound()
+    {
+        foreach (Monkey monkey in _monkeys)
+        {
+            List<long> startingItems = new List<long>();
+            startingItems.AddRange(monkey.StartingItems);
+            monkey.StartingItems.Clear();
+
+            foreach (int startingItem in startingItems)
+            {
+                long worryLevel = monkey.Operation(startingItem);
+                worryLevel = monkey.WorryReducer(worryLevel);
+                int throwTo = monkey.Test(worryLevel);
+                _monkeys[throwTo].StartingItems.Add(worryLevel);
+                monkey.InspectedItems++;
+            }
+        }
+    }
+
+    private class Monkey
+    {
+        public Monkey(
+            List<long> startingItems,
+            Func<long, long> operation,
+            Func<long, int> test)
+        {
+            StartingItems = startingItems;
+            Operation = operation;
+            Test = test;
+        }
+
+        public int InspectedItems { get; set; }
+
+        public List<long> StartingItems { get; }
+
+        public Func<long, long> Operation { get; }
+
+        public Func<long, int> Test { get; }
+
+        public Func<long, long> WorryReducer { get; set; } = null!;
     }
 }
